cec5a33423955e8b390008c3864be6b4ffbbeeb4
retoheusser <heusserreto@gmail.com>
2026-06-05 16:18:50 +0200

    2018

--- data-crawler.js
+++ data-crawler.js
@@ -10,18 +10,18 @@ document.querySelectorAll(".group.flex").forEach((item, index) => {
     const diff = Math.abs(home-away)
     const normalizedResult = [home, away].sort().reverse().join("-")
     const swapped = normalizedResult !== [home, away].join("-")
-    const stage = index > 16 ? "group" : "ko"
+    const stage = index > 15 ? "group" : "ko"
     let round = null
-    if (index > 48) {
+    if (index > 47) {
       round = 1
-    } else if (index > 32) {
+    } else if (index > 31) {
       round = 2
-    } else if (index > 16) {
+    } else if (index > 15) {
       round = 3
     }
     const odds = [data[data.length-3], data[data.length-2], data[data.length-1]].map(Number)
     const probabilitySpan = Math.abs(1/odds[0] - 1/odds[2])
-    const year = 2022
+    const year = 2018
     const minOddIndex = odds.indexOf(Math.min(...odds))
     const oddIsCorrect = (minOddIndex === 0 && home > away) || (minOddIndex === 1 && home === away) || (minOddIndex === 2 && away > home)
     all.push({home, away, total, diff, normalizedResult, swapped, stage, round, odds, probabilitySpan, oddIsCorrect, year})
